57f3833450ef07b5255243ea4d037028251c3273
João Pedro <joaopedro.nc@outlook.com>
2023-01-08 19:59:43 -0300

    ♻ refactor: Global theme

--- src/global/styles/theme.js
+++ src/global/styles/theme.js
@@ -1,17 +1,31 @@
 export default {
   colors: {
-    backgroundColor: "#141a1b",
-    green: "#00FF88",
-    greenSec: "#027558",
-    darkGreen: "#202B2C",
-    red: "#F02323",
-    purple: "#514869",
-    gray: "#868686"
+    background: '#141a1b',
+
+    green_900: '#202B2C',
+    green_700: '#027558',
+    green_500: '#0FE983',
+    green_300: '#00FF88',
+
+    yellow_700: '#8A6A03',
+    yellow_500: '#B08C18',
+    yellow_300: '#FFCE31',
+
+    gray_700: '#868686',
+    gray_500: '#AFB6C2',
+
+    red_700: '#F02323',
+
+    purple_700: '#514869',
+
+    white: '#EBEBEB',
   },
+
   fonts: {
-    regular: "Poppins_400Regular",
-    medium: "Poppins_500Medium",
-    bold: "Poppins_700Bold",
-    semibold: "Poppins_600SemiBold",
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+    semibold: 'Poppins_600SemiBold',
   },
+
 };
