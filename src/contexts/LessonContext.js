6d1e2b0b517e56b598abe6daf2dd6bf3d4932382
0thigs <machinimavlogs@gmail.com>
2023-01-13 00:36:03 -0300

    🧱 ci: Create lesson context

---
+++ src/contexts/LessonContext.js
@@ -0,0 +1,50 @@
+import { createContext, useReducer } from 'react';
+import { questions } from '../utils/questions';
+
+export const LessonContext = createContext();
+
+const stages = ['theory', 'quiz', 'end'];
+
+const initialState = {
+  currentStage: stages[0],
+  questions,
+  currentQuestion: 0,
+  wrongs: 0,
+};
+
+const LessonReducer = (state, action) => {
+  switch (action.type) {
+    case 'changeState':
+      return {
+        ...state,
+        currentStage: stages[1],
+      };
+    case 'reorderQuestions':
+      const reorderedQuestions = questions.sort(() => {
+        return Math.random() - 0.5;
+      });
+      return {
+        ...state,
+        questions: reorderedQuestions,
+      };
+    case 'changeQuestion':
+      const nextQuestion = state.currentQuestion + 1;
+      let isEndQuiz = false;
+      if (!questions[nextQuestion]) {
+        isEndQuiz = true;
+      }
+      return {
+        ...state,
+        currentQuestion: nextQuestion,
+        currentStage: isEndQuiz ? stages[2] : state.currentStage,
+      };
+    default:
+      return state;
+  }
+};
+
+export const LessonProvider = ({ children }) => {
+  const value = useReducer(LessonReducer, initialState);
+
+  return <LessonContext.Provider value={value}>{children}</LessonContext.Provider>;
+};
