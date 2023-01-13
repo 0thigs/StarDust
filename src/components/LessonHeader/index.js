36be46378ed48fe29b8c087f21632c0ae42270f0
0thigs <machinimavlogs@gmail.com>
2023-01-13 00:34:23 -0300

    🚧 build: Create LessonHeader Component

---
+++ src/components/LessonHeader/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as C from './styles';
+
+import CloseButtonIcon from '../../assets/GlobalAssets/close-button-icon.svg';
+import LiveIcon from '../../assets/GlobalAssets/heart.svg';
+import Rocket from '../../assets/GlobalAssets/rocket.png';
+
+export function LessonHeader() {
+  return (
+    <>
+      <C.Main>
+        <C.CloseButton>
+          <CloseButtonIcon width={35} height={35} />
+        </C.CloseButton>
+        <C.Lives>
+          <LiveIcon width={30} height={30} />
+          <C.LivesCount>5</C.LivesCount>
+        </C.Lives>
+      </C.Main>
+      <C.ProgressBar>
+        <C.Bar>
+          <C.Rocket source={Rocket} />
+        </C.Bar>
+      </C.ProgressBar>
+    </>
+  );
+}
