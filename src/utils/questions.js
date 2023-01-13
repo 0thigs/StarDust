7f275434a9c413aa62a7e8060c2689dcb0b5e7bd
0thigs <machinimavlogs@gmail.com>
2023-01-13 00:20:16 -0300

    ✨ feat: Questions.js

---
+++ src/utils/questions.js
@@ -0,0 +1,79 @@
+export const questions = [
+    {
+      starId: 1,
+      type: 'select-option',
+      stem: 'Dentro das opções que não se enquadra na formação de algoritmos é',
+      options: ['raciocínio lógico', 'aleatoriedade', 'análise crítica', 'encadeamento de passos'],
+      answer: 'aleatoriedade',
+    },
+    {
+      starId: 1,
+      type: 'open',
+      stem: 'O nome que se dá a uma sequencia lógica de instruções é',
+      answer: 'alroritmo',
+    },
+    {
+      starId: 1,
+      type: 'select-option',
+      stem: 'O que é lógica de programação?',
+      options: [
+        'sequência lógica de instruções',
+        'fazer programa',
+        'maneira de pensar um algoritmo',
+        'criar problemas computacionais',
+      ],
+      answer: 'aleatoriedade',
+    },
+    {
+      starId: 1,
+      type: 'drag-and-drop-list',
+      stem: 'Coloque o algoritmo na ordem, de modo que fique logicamente correto',
+      options: [
+        {
+          id: 1,
+          label: 'mascar o chiclete',
+        },
+        {
+          id: 2,
+          label: 'retirar embalagem do chiclete',
+        },
+        {
+          id: 3,
+          label: 'jogar o chiclete no lixo',
+        },
+        {
+          id: 4,
+          label: 'colocar chiclete na boca',
+        },
+      ],
+      correctItemsSequence: [2, 4, 1, 3],
+    },
+    {
+      starId: 1,
+      type: 'drag-and-drop-list',
+      stem: 'Coloque os elementos na sequência correta para formar um algoritmo',
+      options: [
+        {
+          id: 1,
+          label: 'ir para o trabalho',
+        },
+        {
+          id: 2,
+          label: 'tomar banho',
+        },
+        {
+          id: 3,
+          label: 'acordar',
+        },
+        {
+          id: 4,
+          label: 'tomar café',
+        },
+        {
+          id: 5,
+          label: 'tirar o carro da garagem',
+        },
+      ],
+      correctItemsSequence: [3, 2, 4, 5, 1],
+    },
+  ];
