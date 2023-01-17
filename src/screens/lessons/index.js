8e2705dd3cfa6637ee9ec7385fd7a62f9c11cc71
0thigs <machinimavlogs@gmail.com>
2023-01-16 21:52:43 -0300

    ♻ Refactor: Update Lessons and Api Services

--- src/screens/lessons/index.js
+++ src/screens/lessons/index.js
@@ -1,22 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as C from './styles';
 import { useLesson } from '../../hooks/useLesson';
 import { Theory } from '../../components/Theory';
 import { Quiz } from '../../components/Quiz';
 import { End } from '../../components/End';
+import { TransitionScreenAnimation } from '../../components/TransitionScreenAnimation';
+import { useAuth } from '../../hooks/useAuth';
 
 export function Lesson() {
+  const { user } = useAuth();
   const [state, dispatch] = useLesson();
+  const [isEndTrasition, setIsEndTransition] = useState(false);
 
   useEffect(() => {
     dispatch({ type: 'reorderQuestions' });
+    setTimeout(() => setIsEndTransition(true), 3000);
   }, []);
 
   return (
     <C.Container>
-      {state.currentStage === 'theory' && <Theory />}
-      {state.currentStage === 'quiz' && <Quiz />}
-      {state.currentStage === 'end' && <End />}
+      {!isEndTrasition ? (
+        <TransitionScreenAnimation />
+      ) : (
+        <>
+          {state.currentStage === 'theory' && <Theory starId={user.starId} />}
+          {state.currentStage === 'quiz' && <Quiz />}
+          {state.currentStage === 'end' && <End starId={user.starId} />}
+        </>
+      )}
     </C.Container>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,3 @@
-import { supabase } from './supabase';
-
 export default {
   addUser: async ({ id, name, email }) => {
     const { data, error } = await supabase.from('user').insert([{ id, name, email }]);
@@ -7,5 +5,65 @@ export default {
       return error.message;
     }
     return data;
+  },
+
+  getUser: async userId => {
+    const { data, error, status } = await supabase
+      .from('users')
+      .select('name, email, unlockedStarsIds, coins, lives')
+      .eq('id', userId)
+      .limit(1);
+    if (error) {
+      return error.message;
+    }
+    return data[0];
+  },
+
+  getPlanets: async () => {
+    const { data: planets, error } = await supabase
+      .from('planets')
+      .select('*')
+      .order('id', { ascending: true });
+    if (error) {
+      return error.message;
+    }
+    return planets;
+  },
+
+  getStars: async () => {
+    const { data: stars, error } = await supabase.from('stars').select('*');
+    if (error) {
+      return error.message;
+    }
+    return stars;
+  },
+
+  updateLives: async (lives, userId) => {
+    const { error } = await supabase.from('users').update({ lives }).eq('id', userId);
+    if (error) {
+        console.log(error);
+      return error.message;
+    }
+  },
+
+  updateCoins: async (coins, userId) => {
+    const { error } = await supabase.from('users').update({ coins }).eq('id', userId);
+    if (error) {
+      return error.message;
+    }
+  },
+
+  updateXp: async (xp, userId) => {
+    const { error } = await supabase.from('users').update({ xp }).eq('id', userId);
+    if (error) {
+      return error.message;
+    }
+  },
+
+  updateUnlockedStarsIds: async (unlockedStarsIds, userId) => {
+    const { error } = await supabase.from('users').update({ unlockedStarsIds }).eq('id', userId);
+    if (error) {
+      return error.message;
+    }
   },
 };
