ba91e86c868c04747896d23b9e3b6646da4cdcec
0thigs <machinimavlogs@gmail.com>
2023-01-16 22:08:13 -0300

    💫 Refactor: Add Animation Planet

--- src/components/Planet/index.js
+++ src/components/Planet/index.js
@@ -1,27 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as C from './styles';
 
 import { Star } from '../Star';
 import { planetImages } from '../../utils/PlanetsImages';
 import { planetIcons } from '../../utils/PlanetIcons';
 
-export function Planet({ title, id }) {
+import {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+  Easing,
+  withDelay,
+} from 'react-native-reanimated';
+
+export function Planet({ title, id, currentPlanetStarsIds, stars }) {
+  const [currentPlanetStars, setCurrentPlanetStars] = useState([]);
+
   const PlanetImage = planetImages['planet' + id];
   const PlanetIcon = planetIcons['planet' + id];
+
+  const PlanetSignPosition = useSharedValue(-5);
+  const PlanetInfoScale = useSharedValue(0);
+
+  const enabledStars = currentPlanetStars.filter(star => star.isUnlocked)
+
+  function getCurrentPlanetStars() {
+    const planetStars = stars.filter(star => currentPlanetStarsIds.includes(star.id));
+    setCurrentPlanetStars(planetStars);
+  }
+
+  const PlanetInfoAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: PlanetInfoScale.value }],
+    };
+  });
+
+  const PlanetSignAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: PlanetSignPosition.value }],
+    };
+  });
+
+  useEffect(() => {
+    PlanetSignPosition.value = withDelay(800, withRepeat(withTiming(5, { duration: 1000 }), -1, true));
+    PlanetInfoScale.value = withTiming(1, { duration: 800, easing: Easing.bounce });
+    getCurrentPlanetStars();
+  }, []);
 
   return (
     <C.Container>
       <C.PlanetInfo>
         <PlanetImage width={100} height={100} />
-        <C.PlanetSign>
+        <C.PlanetSign style={PlanetSignAnimatedStyle}>
           <PlanetIcon />
-          <C.PlanetName>{title}</C.PlanetName>
+          <C.PlanetTitle>{title}</C.PlanetTitle>
         </C.PlanetSign>
       </C.PlanetInfo>
-      <Star name={'Conceito de lógica'} number={1} isDisabled={false} />
-      <Star name={'O que é um programa'} number={2} isDisabled={true} />
-      <Star name={'Entrada, processamento e saída'} number={3} isDisabled={true} />
-      <Star name={'Entrada, processamento e saída'} number={4} isDisabled={true} />
+      <C.StarsList
+        data={currentPlanetStars}
+        keyExtractor={star => star.id}
+        renderItem={({ item, index }) => {
+          const isTheLastStarEnabled = index === enabledStars.length - 1;
+          return (
+            <Star
+              id={item.id}
+              name={item.title}
+              number={item.number}
+              isDisabled={!item.isUnlocked}
+              isTheLastStarEnabled={isTheLastStarEnabled}
+            />
+          );
+        }}
+      />
     </C.Container>
   );
 }
--- src/components/Planet/styles.js
+++ src/components/Planet/styles.js
@@ -1,17 +1,18 @@
 import styled from 'styled-components/native';
+import Animated from 'react-native-reanimated';
 
 export const Container = styled.View`
-    width: 300px;
-    margin-bottom: 40px;
+  width: 300px;
+  margin-bottom: 40px;
 `;
 
-export const PlanetInfo = styled.View`
+export const PlanetInfo = styled(Animated.View)`
   flex-direction: row;
   padding: 0 4px;
   align-items: center;
 `;
 
-export const PlanetSign = styled.View`
+export const PlanetSign = styled(Animated.View)`
   background-color: ${props => props.theme.colors.green_700};
   border-radius: 10px;
   padding: 0 12px;
@@ -22,14 +23,11 @@ export const PlanetSign = styled.View`
   height: 64px;
 `;
 
-export const PlanetName = styled.Text`
+export const PlanetTitle = styled.Text`
   color: ${props => props.theme.colors.white};
   font-family: ${props => props.theme.fonts.bold};
   font-size: 14px;
   margin-left: 4px;
 `;
 
-export const StarDust = styled.View`
-  width: 100%;
-  padding-left: 40px;
-`;
+export const StarsList = styled.FlatList``;
