2e0ec127ecbfc4c833698fdfd4d537683d95fa00
0thigs <machinimavlogs@gmail.com>
2023-01-25 01:29:57 -0300

    ✨ Feat: Create Modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,33 @@
+import * as C from './styles';
+
+import Crying from '../../assets/ModalAssets/crying-animation.json';
+import Denying from '../../assets/ModalAssets/denying-animation.json';
+import Earning from '../../assets/ModalAssets/earning-animation.json';
+
+
+export function Modal({ show, type = 'crying', title, body, footer }) {
+  const animations = [
+    { id: 'crying', animation: Crying },
+    { id: 'denying', animation: Denying },
+    { id: 'earning', animation: Earning },
+  ];
+
+  function getAnimation() {
+    return animations.find(animation => animation.id === type.toLocaleLowerCase()).animation;
+  }
+
+  return (
+    <C.Container transparent visible={show} animationType={'fadeIn'}>
+      <C.Fade>
+        <C.Content animation={'flipInX'} duration={10}>
+          <C.Header>
+            <C.Title>{title}</C.Title>
+            <C.Animation source={getAnimation()} autoPlay={true} loop={true} />
+          </C.Header>
+          <C.Body>{body}</C.Body>
+          <C.Footer>{footer}</C.Footer>
+        </C.Content>
+      </C.Fade>
+    </C.Container>
+  );
+}
---
+++ src/components/Modal/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components/native';
+import LottieView from 'lottie-react-native';
+import * as Animatable from 'react-native-animatable';
+
+export const Container = styled.Modal``;
+
+export const Fade = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.1);
+`;
+
+export const Content = styled(Animatable.View)`
+  width: 90%;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  background-color: ${props => props.theme.colors.blue_700};
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  padding: 16px 0 0;
+`;
+
+export const Animation = styled(LottieView)`
+  width: 170px;
+  height: 170px;
+`;
+
+export const Title = styled.Text`
+  color: ${props => props.theme.colors.white};
+  font-family: ${props => props.theme.fonts.bold};
+  font-size: 16px;
+  text-align: center;
+`;
+
+export const Body = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding: 8px 0 8px;
+  background-color: ${props => props.theme.colors.black};
+`;
+
+export const Footer = styled.View`
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  padding: 8px;
+  background-color: ${props => props.theme.colors.black};
+`;
